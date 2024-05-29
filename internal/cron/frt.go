080e81f2eec124f70b45c74334149dd068b99d28
Sandeep Kumar <sandepten@gmail.com>
2024-05-29 10:03:18 +0530

    Refactor cron jobs to use cron package and sync data from Peoplestrong database

--- internal/cron/frt.go
+++ internal/cron/frt.go
@@ -1,6 +1,21 @@
 package cron
 
-import "attendance/internal/database"
+import (
+	"attendance/internal/database"
+
+	"github.com/robfig/cron"
+)
+
+func FRTCron() {
+	c := cron.New()
+
+	// Run the sync every 5 minutes
+	c.AddFunc("*/5 * * * *", func() {
+		SyncAwsFrtDataCron()
+	})
+
+	// c.Start()
+}
 
 func SyncAwsFrtDataCron() {
 	maxId := database.FetchFRTMaxFetchId()
--- internal/cron/peoplestrong.go
+++ internal/cron/peoplestrong.go
@@ -1,6 +1,21 @@
 package cron
 
-import "attendance/internal/database"
+import (
+	"attendance/internal/database"
+
+	"github.com/robfig/cron"
+)
+
+func PeoplestrongCron() {
+	c := cron.New()
+
+	// Run the sync every 30 minutes
+	c.AddFunc("*/30 * * * *", func() {
+		SyncFrtLogsPeoplestrongCron()
+	})
+
+	// c.Start()
+}
 
 func SyncFrtLogsPeoplestrongCron() {
 	lastPushedId := database.LastPushedId()
