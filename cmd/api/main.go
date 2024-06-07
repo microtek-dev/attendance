3ec4a3229515f4cd526f11d811ea42c37987ab16
Sandeep Kumar <sandepten@gmail.com>
2024-06-07 12:14:37 +0530

    fix: sales time issues while pushing to aws

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,22 +1,17 @@
 package main
 
 import (
-	"attendance/internal/database"
+	"attendance/internal/cron"
 	"attendance/internal/server"
 	"fmt"
-	"time"
 )
 
 func main() {
 	server := server.NewServer()
 
-	// cron.FRTCron()
-	// cron.PeoplestrongCron()
-	// cron.SalesCron()
-
-	// set date as 29 May 2024 in string format
-	date := time.Date(2024, time.May, 29, 0, 0, 0, 0, time.UTC).Format("2006-01-02")
-	database.SyncSalesAttendanceFromFieldAssist(date)
+	cron.FRTCron()
+	cron.PeoplestrongCron()
+	cron.SalesCron()
 
 	err := server.ListenAndServe()
 	if err != nil {
