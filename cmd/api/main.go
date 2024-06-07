66f75fc172d13a4ef77ce0e0ca77d63f57f01404
Sandeep Kumar <sandepten@gmail.com>
2024-06-07 10:17:53 +0530

    fix the timezone issues of sales attendance

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
-	"attendance/internal/cron"
+	"attendance/internal/database"
 	"attendance/internal/server"
 	"fmt"
+	"time"
 )
 
 func main() {
 	server := server.NewServer()
 
-	cron.FRTCron()
-	cron.PeoplestrongCron()
-	cron.SalesCron()
+	// cron.FRTCron()
+	// cron.PeoplestrongCron()
+	// cron.SalesCron()
+
+	// set date as 29 May 2024 in string format
+	date := time.Date(2024, time.May, 29, 0, 0, 0, 0, time.UTC).Format("2006-01-02")
+	database.SyncSalesAttendanceFromFieldAssist(date)
 
 	err := server.ListenAndServe()
 	if err != nil {
